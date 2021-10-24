6f3ee676edce9eeff43d7aca8c8400cbb95bd351
unknown-cat <twilightcat73@yahoo.com>
2021-10-24 20:22:35 +0300

    feat: add redux-thunk
    create async action for fetching data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
 import { createLogger } from "redux-logger";
-import { searchRobots } from "./reducers";
+import thunkMiddleware from "redux-thunk"
+import { searchRobots, requestRobots } from "./reducers";
 import registerServiceWorker from './registerServiceWorker';
 import App from './containers/App';
 import 'tachyons';
 import './index.css';
 
 const logger = createLogger()
-const store = createStore(searchRobots, applyMiddleware(logger))
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
 	<Provider store={ store }>
