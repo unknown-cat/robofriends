a98f9feff09d24e02ce930d6ed30fe15b8202d7e
unknown-cat <twilightcat73@yahoo.com>
2021-11-15 21:42:09 +0300

    test: create test for filterRobots method in MainPage component
    test if filter robots correctly

--- src/components/MainPage.test.js
+++ src/components/MainPage.test.js
@@ -17,3 +17,22 @@ beforeEach(() => {
 it('renders mainPage', () => {
 	expect(wrapper).toMatchSnapshot()
 })
+
+it('filters robots correctly', () => {
+	const mockProps2 = {
+		onRequestRobots: jest.fn(),
+		robots: [{
+			id: 3,
+			name: 'john',
+			email: 'john@gmail.com'
+		}],
+		searchField: 'john',
+		pending: false
+	}
+	const wrapper2 = shallow(<MainPage {...mockProps2}/>)
+	expect(wrapper2.instance().filterRobots()).toEqual([{
+		id: 3,
+		name: 'john',
+		email: 'john@gmail.com'
+	}])
+})
